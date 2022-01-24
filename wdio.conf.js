44b892f23aca67f84bf2af3e4465a4c7d7cc4d81
Thiago Cesar <andrade-tc@uol.com.br>
2022-01-24 20:14:07 -0300

    Inicializando o repositorio

---
+++ wdio.conf.js
@@ -0,0 +1,21 @@
+const {join} = require('path')
+
+exports.config = {
+   hostname: 'localhost',
+   port: 4723,
+   path: '/wd/hub',
+   specs: [
+       './test/specs/**/*.js'
+   ],
+   framework: 'mocha',
+   capabilities:
+   [{
+    "platformName": "Android",
+    "platformVersion": "8.1",
+    "deviceName": "ebac-qe",
+    "automationName": "UiAutomator2",
+    "app": join(process.cwd(), './app/android/loja-ebac.apk'),
+    "appWaitActivity": 'com.woocommerce.android/.ui.login.LoginActivity'
+   }]
+
+}
